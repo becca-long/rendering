2420e83d67bae909f1e4fb637cb9e7c589a3097b
Becca Randall <becca.m.randall@gmail.com>
2018-10-26 14:56:02 -0500

    Solved part of renderStudents but still working on for loop

--- renderStudents.js
+++ renderStudents.js
@@ -1,11 +1,36 @@
 
 function renderStudents(students) {
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(students)}</code>
+    var finalHTML = '<div class="container">Roll Call!</div>';
+
+    // var studentAttendance = []
+    
+    // for (i = 0; i < `${students.length}`; i++) {
+    //     if (`${students.isPresent}` = true) {
+    //         studentAttendance.push('Present');
+    //     } else {
+    //         studentAttendance.push('Absent');
+    //     }
+    // }
+
+    // return studentAttendance
+    
+    var studentsHTML = students.map(function (student) {
+        var student = `
+        <div class='student'>
+        <h3>${student.name}</h3>
+        <b>${student.isPresent}</b>
         </div>
-    `
+        `
+        return student
+    }).join('')
+    
+    finalHTML += studentsHTML
+    
+    return finalHTML
 }
+
+
+
 
 function students() {
     var content = document.getElementById('content');
